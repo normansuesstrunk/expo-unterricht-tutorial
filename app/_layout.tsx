e3b99e75667025c876ce2bf8685eea5bb31aa0a7
Norman Süsstrunk <n.suesstrunk@gmail.com>
2025-09-09 10:49:17 +0200

    added tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,33 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
 
-export default function RootLayout() {
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+export default function TabLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
-    </Stack>
-  )
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#ffd33d',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from 'expo-router';
 
+
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-       <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
